c82fd3e6eb430e8e445aec2ca17ff1296a8cbfd1
Vlad Rvk <rvk.vlad@gmail.com>
2015-08-29 00:04:24 -0700

    Revert "Deleted Forlder Testjunit"
    This reverts commit df604775676e64a71952ca7cf3d2092739be85c7.

---
+++ Tests/Testjunit/Mytest.java
@@ -0,0 +1,23 @@
+package Testjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Mytest {
+	WebDriver driver = new FirefoxDriver();
+
+	@Test
+	public void test() {
+		driver.manage().window().maximize();
+		System.out.println("Window maximize");
+		driver.get("http://only-testing-blog.blogspot.in/");
+		System.out.print("Site open");
+		driver.quit();
+		System.out.println("End of Test");
+		
+	}
+
+}
